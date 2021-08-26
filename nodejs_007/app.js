e5454be787ccb1b1f232c30f037bac400592fbb1
choiseonyoung <bviibb@naver.com>
2021-08-26 16:18:52 +0900

    file

--- nodejs_007/app.js
+++ nodejs_007/app.js
@@ -8,6 +8,7 @@ var app = express();
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 const bbsRouter = require("./routes/bbsRouter");
+const fileRouter = require("./routes/fileRouter");
 
 const sequelize = require("./models/index").sequelize.sync();
 // .sequelize.sync() : tbl_bbs.js 등에 설정된 정보를 기준으로 table이 없으면 table을 자동으로 생성해준다
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/bbs", bbsRouter);
+app.use("/file", fileRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
